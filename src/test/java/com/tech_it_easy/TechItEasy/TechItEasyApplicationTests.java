524f48187d6b48ed6e060b0568d56370a7f27a05
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2024-11-21 14:09:20 +0100

    boilerplate code SpringInitializer

---
+++ src/test/java/com/tech_it_easy/TechItEasy/TechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_it_easy.TechItEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
